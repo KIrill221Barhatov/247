6e27cb122fe96d54fdf4b46a303e9542aa73b340
Кирилл Бархатов <117072370+KIrill221Barhatov@users.noreply.github.com>
2023-02-07 17:24:40 +0500

    Add files via upload

---
+++ 247 - 253/index.js
@@ -0,0 +1,47 @@
+// 247.1
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', func1);
+// function func1() {
+//     console.log(elem.textContent);
+// }
+// 247.2
+// let elem = document.querySelector('#elem')
+// let button = document.querySelector('#button')
+// button.addEventListener('click', func1);
+// function func1() {
+//     console.log(elem.textContent)
+//     elem.textContent = 'text1'
+// }
+// 247.3
+// let elem1 = document.querySelector('#elem1')
+// let elem2 = document.querySelector('#elem2')
+// let button = document.querySelector('#button')
+// button.addEventListener('click', func1)
+// function func1() {
+//     console.log(Number(elem1.textContent) + Number(elem2.textContent))
+// }
+// 247.4
+// let elem1 = document.querySelector('#elem1')
+// let elem2 = document.querySelector('#elem2')
+// let elem3 = document.querySelector('#elem3')
+// let div = document.querySelector('#div')
+// let button = document.querySelector('#button')
+// button.addEventListener('click', function() {
+// console.log(div = Number(elem1.textContent) + Number(elem2.textContent) + Number(elem3.textContent))
+// })
+// 247.5
+// let elem = document.querySelector('#elem')
+// let button = document.querySelector('#button')
+// button.addEventListener('click', func1)
+// function func1() {
+//     console.log(Number(elem.textContent) + 1)
+//     console.log(Number(elem.textContent))
+// }
+// 247.6
+let elem = document.querySelector('#elem')
+let button = document.querySelector('#button')
+button.addEventListener('click', function () {
+    console.log(elem.textContent + '!!!')
+})
+
